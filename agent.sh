a54a04f916403df29622c72a8d8b4c56f4d1791b
Charlene Lopez Taw <tawcharlene@gmail.com>
2015-02-26 20:55:25 +0800

    ssh key

--- agent.sh
+++
@@ -1,3 +0,0 @@
-SSH_AUTH_SOCK=/var/folders/pn/ntpwglpd0_71dn9lqxnx1rq40000gn/T//ssh-xvRsoWUWs9i9/agent.12644; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=12645; export SSH_AGENT_PID;
-echo Agent pid 12645;
